bcfa5b0702269b3c9f7f91fde3b4efd9bee380ff
stevenleEPIC <leqsteven@yahoo.com>
2026-09-14 14:08:38 -0600

    Add files via upload

--- dimmable.py
+++ dimmable.py
@@ -1,7 +1,7 @@
 from gpiozero import RotaryEncoder, PWMLED
 from time import sleep
 
-rotor = RotaryEncoder(17, 18, wrap=True, max_steps=75) # DT goes to 16, CLK goes to 20
+rotor = RotaryEncoder(17, 18, wrap=True, max_steps=180) # DT goes to 16, CLK goes to 20
 
 
 led = PWMLED(24)
@@ -9,8 +9,8 @@ led = PWMLED(24)
 while True:
 	if rotor.steps<0 and rotor.steps>-25:
 		rotor.steps=rotor.steps*0 # makes the value so it can't go below -1
-	elif rotor.steps>=-75 and rotor.steps<-50:
-		rotor.steps=75
-	led.value = rotor.steps / 75 # the value of led. value will change as you rotate the device
+	elif rotor.steps>=-180 and rotor.steps<-50:
+		rotor.steps=180 # makes the value so it's can't go to -180
+	led.value = rotor.steps / 180 # the value of led. value will change as you rotate the device
 	sleep(0.1) 
 	print(rotor.steps) # the value of rotor.steps will change as you rotate the device
